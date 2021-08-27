37120084c16424ced4bad9cfe8c0686a5248a6d9
17965 <1796520046@qq.com>
2021-08-27 09:48:45 +0800

    初始化提交，使用Ruoyi单体版v4.6.2脚手架，修改包名以及登录页面前端

---
+++ ikea-common/src/main/java/com/ikea/common/enums/DataSourceType.java
@@ -0,0 +1,19 @@
+package com.ikea.common.enums;
+
+/**
+ * 数据源
+ * 
+ * @author ruoyi
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
